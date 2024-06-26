2f13f9adc42b0ccf6aabd53cd778dbe9a94640c3
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2024-06-27 00:23:51 +0200

    template OK - custom WIP

---
+++ app.js
@@ -0,0 +1,71 @@
+// Step 1: Get DOM elements
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+
+let carouselDom = document.querySelector(".carousel");
+let SliderDom = carouselDom.querySelector(".carousel .list");
+let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+let timeDom = document.querySelector(".carousel .time");
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 30000; // 30 seconds for auto-slide
+
+nextDom.onclick = function () {
+  showSlider("next");
+  resetAutoSlideTimer();
+};
+
+prevDom.onclick = function () {
+  showSlider("prev");
+  resetAutoSlideTimer();
+};
+
+let runTimeOut;
+let autoSlideExecuted = false; // Flag to check if the auto-slide has been executed
+
+let runNextAuto = setTimeout(() => {
+  if (!autoSlideExecuted) {
+    nextDom.click();
+    autoSlideExecuted = true;
+  }
+}, timeAutoNext);
+
+function showSlider(type) {
+  let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+  let thumbnailItemsDom = document.querySelectorAll(".carousel .thumbnail .item");
+
+  if (type === "next") {
+    SliderDom.appendChild(SliderItemsDom[0]);
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    carouselDom.classList.add("next");
+  } else {
+    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+    carouselDom.classList.add("prev");
+  }
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("next");
+    carouselDom.classList.remove("prev");
+  }, timeRunning);
+
+  clearTimeout(runNextAuto);
+  runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext);
+}
+
+// Function to reset the auto-slide timer
+function resetAutoSlideTimer() {
+  clearTimeout(runNextAuto);
+  if (!autoSlideExecuted) {
+    runNextAuto = setTimeout(() => {
+      if (!autoSlideExecuted) {
+        nextDom.click();
+        autoSlideExecuted = true;
+      }
+    }, timeAutoNext);
+  }
+}
